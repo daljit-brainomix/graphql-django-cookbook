1237cb925cf3eb07779fa5ecfe9462539f0334b8
Daljit Singh <dsingh@brainomix.com>
2022-10-10 11:34:59 +0100

    Just demo

---
+++ ingredients/schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene_django import DjangoObjectType
+from ingredients.models import Category, Ingredient
+
+# A "Type" is an object that represents your model,
+# you can tailor it to let you filter your results based on a criteria.
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        # By default DjangoObjectType will present all fields on a Model through GraphQL
+        # Use fields() or exclude() - include or exclude fields.
+        fields = ("id", "name", "ingredients")
+
+
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        fields = ("id", "name", "notes", "category")
+
+    # To add and resolve any extra field not present in the model
+    # extra_field = graphene.String()
+    # def resolve_extra_field(self, info):
+    #     return "hello!"
+
+
+class Query(graphene.ObjectType):
+    all_ingredients = graphene.List(IngredientType)
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
+
+    def resolve_all_ingredients(root, info):
+        return Ingredient.objects.select_related("category").all()
+
+    def resolve_category_by_name(root, info, name):
+        try:
+            # Querying category by name
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,47 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+
+import requests
+import json
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!!")
+
+
+def show(request):
+    # api_url = request.build_absolute_uri(reverse("graphql",))
+    api_url = "http://127.0.0.1:8000/graphql"
+
+    graphql_query = """
+    {
+     allIngredients {
+         id
+         name
+     }   
+    }
+    """
+    payload = {"query": graphql_query}
+    response = requests.post(api_url, json=payload)
+    data = response.json()
+
+    print("===" * 10)
+    print(data["data"]["allIngredients"][0]["name"])
+
+    return HttpResponse(data)
+
+
+"""
+query {
+  allIngredients {
+    id
+    name
+    category {
+      id
+      name
+    }
+  }
+}
+
+"""
